7a9cc49e210a7908746e9167845db84b80f41922
Surya Narendra Reddy <suryanarendra1000@gmail.com>
2025-12-19 15:23:22 +0530

    Ready for deploy

---
+++ backend/api/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('../config/db')
+
+dotenv.config()
+
+const app = express()
+
+app.use(
+  cors({
+    origin: 'https://engineersworld.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  })
+)
+
+app.use(express.json())
+
+connectDB()
+
+app.use('/api/contact', require('../routes/contactRoutes'))
+app.use('/api/jobs', require('../routes/jobApplicationRoutes'))
+app.use('/api/internships', require('../routes/internshipRoutes'))
+
+module.exports = app
