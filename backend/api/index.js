215e19bc6c5ec2171b5042636edd9a9a1904250e
Surya Narendra Reddy <suryanarendra1000@gmail.com>
2025-12-20 11:31:03 +0530

    Everything is ready

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URI],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
