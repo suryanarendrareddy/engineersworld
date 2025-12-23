5eaabd4a11b9682220e6c6537ae9d88eab7f6ce5
Muli Surya Narendra Reddy <suryanarendra1000@gmail.com>
2025-12-23 12:39:07 +0530

    Vite config is commited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base:'/',
   build: {
     outDir: 'dist',
     rollupOptions: {
